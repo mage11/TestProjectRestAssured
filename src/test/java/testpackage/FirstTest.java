80df943c2eee2d8db219d07ba71cb54e2a10f92c
mage <petya11@mail.ru>
2020-07-16 14:40:45 +0300

    Добавлена HOCON-конфигурация

--- src/test/java/testpackage/FirstTest.java
+++ src/test/java/testpackage/FirstTest.java
@@ -1,11 +1,13 @@
 package testpackage;
 
+import com.typesafe.config.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
@@ -14,46 +16,65 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class FirstTest {
 
-    private static String login = "XXX";
-    private static String password = "XXX";
+    private static String baseLogin;
+    private static String basePassword;
+    private static String frontURL;
+    private static String authURL;
+    private static String cookie;
+    private static String cookieName;
 
+    //Выполняется перед тестом. Получаем необходимые переменные из test.conf
+    @BeforeTest
+    public void setUp(){
+
+        Config conf = ConfigFactory.load("test");
+
+        baseLogin = conf.getString("baseLogin");
+        basePassword = conf.getString("basePassword");
+        frontURL = conf.getString("front");
+        authURL = conf.getString("auth");
+        cookieName = conf.getString("cookieName");
+
+        cookie = getCoockie();
+
+
+    }
+
+    //Проверка версии
     @Test()
     public void getRequestExampleTest() throws JSONException {
 
         String ver = "0.21.0\n";
 
         given()
-                .get("http://id-test2.entera.pro/api/version")
+                .get(authURL + "/api/version")
                 .then().assertThat().contentType(ContentType.TEXT)
                 .assertThat().body(equalTo(ver));
     }
 
+    //Проверка /impersonate
     @Test
     public void login() {
 
-        String jwt = getCoockie();
-
-
         given().contentType(ContentType.JSON)
-                .cookie("ENTERA_JWT_TEST", jwt)
+                .cookie("ENTERA_JWT_TEST", cookie)
                 .body("{\"login\": \"entera_test@mail.ru\"}")
-                .post("https://id-test2.entera.pro/api/v1/impersonate")
+                .post(authURL + "/api/v1/impersonate")
                 .then().body("result", equalTo(true));
 
     }
 
     private String getCoockie(){
-        String URL = "https://id-test2.entera.pro/api/v1/login";
 
         JSONObject json = new JSONObject();
-        json.put("login", login);
-        json.put("password", password);
+        json.put("login", baseLogin);
+        json.put("password", basePassword);
 
         Response response=  given()
-                .header("Content-Type","application/json")
+                .contentType(ContentType.JSON)
                 .body(json.toString())
-                .post(URL);
-        return response.getCookie("ENTERA_JWT_TEST");
+                .post(authURL + "/api/v1/login");
+        return response.getCookie(cookieName);
     }
 
 }
